0182340ff693003aafedfc5c490af0d247ab2bbc
Ekko <2487242076@qq.com>
2022-08-05 17:42:53 +0800

    2022.08.05

--- src/main.js
+++ src/main.js
@@ -7,13 +7,10 @@ import * as Icons from '@element-plus/icons-vue';
 import axios from 'axios';
 import router from './router/index';
 // import store from "./store/index";
-// import * as echarts from 'echarts';
 
 const app = createApp(App)
 //Vue3.0中不支持Vue.prototype的方式来挂载axios了，改用下面的方法挂载
 app.config.globalProperties.$http = axios;
-// app.config.globalProperties.$echarts = echarts
-// app.component('Button',Button);
 app.use(router)
 // app.use(store)
 app.use(ElementPlus)
